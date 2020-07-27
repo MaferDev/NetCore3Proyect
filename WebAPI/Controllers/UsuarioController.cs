c20ddd6473edff441ebc5ca09a3820752361bc77
MaryFerH <fer.hs19@gmail.com>
2020-07-27 00:05:08 -0400

    Creación del controller Login

---
+++ WebAPI/Controllers/UsuarioController.cs
@@ -0,0 +1,17 @@
+using System.Threading.Tasks;
+using Aplicacion.Seguridad;
+using Dominio;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers
+{
+    public class UsuarioController:MyControllerBase
+    {
+        //htttp://localhosto:5000/api/usuario/login
+        [HttpPost("login")]
+        public async Task<ActionResult<Usuario>> Login(Login.Ejecuta parametros)
+        {
+            return await Mediator.Send(parametros);
+        }
+    }
+}
